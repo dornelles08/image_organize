fc08bc43f77865f003f891e9eb80b9a012d6f40a
Felipe Dornelles <lipe.dornelles@hotmail.com>
2023-10-18 12:06:43 -0300

    try except to view errors

--- separet_images_has_person.py
+++ separet_images_has_person.py
@@ -11,24 +11,29 @@ images_without_person = f"{path}/images_without_person"
 
 images = os.listdir(path)
 
+print(images)
+
 os.makedirs(name=images_with_person, exist_ok=True)
 os.makedirs(name=images_without_person, exist_ok=True)
 
 pbar_images = tqdm(images, desc="Processing")
 
 for image in pbar_images:
-    if os.path.isdir(image):
-        continue
+    try:
+        if os.path.isdir(image):
+            continue
 
-    people = person_detection(f"{path}/{image}")
+        people = person_detection(f"{path}/{image}")
 
-    if (people > 0):
-        shutil.move(
-            f"{path}/{image}",
-            images_with_person
-        )
-    else:
-        shutil.move(
-            f"{path}/{image}",
-            images_without_person
-        )
+        if (people > 0):
+            shutil.move(
+                f"{path}/{image}",
+                images_with_person
+            )
+        else:
+            shutil.move(
+                f"{path}/{image}",
+                images_without_person
+            )
+    except Exception as e:
+        print(e)
